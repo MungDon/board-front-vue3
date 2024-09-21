861b6280d02d00894cc10aaa40ce9e6869006309
MungDon <sungminhyeong@naver.com>
2024-09-22 05:02:20 +0900

    proxyServerSet

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,11 @@
-const { defineConfig } = require('@vue/cli-service')
-module.exports = defineConfig({
-  transpileDependencies: true
-})
+/*const { defineConfig } = require('@vue/cli-service');*/
+module.exports = {
+  devServer: {
+    port: 3000,
+    proxy: {
+      '/api': {
+        target: 'http://localhost:8080'
+      }
+    }
+  }
+};
