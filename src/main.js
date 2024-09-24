10af1adab62cbc696db41ea946f92f665c34ad5f
MungDon <sungminhyeong@naver.com>
2024-09-24 12:21:58 +0900

    global SwalCall 적용

--- src/main.js
+++ src/main.js
@@ -4,8 +4,29 @@ import router from './router';
 import './assets/css/init.css';
 import { QuillEditor } from '@vueup/vue-quill';
 import '@vueup/vue-quill/dist/vue-quill.snow.css';
-
+import Swal from 'sweetalert2';
 const app = createApp(App);
+
+const defaultThenFn = () => {
+  return;
+};
+
+app.config.globalProperties.$swalCall = (swalObj) => {
+  Swal.fire({
+    title: swalObj.title,
+    html: swalObj.text,
+    icon: swalObj.icon,
+    confirmButtonText: swalObj.confirmButtonText ?? '확인',
+    showCancelButton: swalObj.showCancelButton ?? false,
+    cancelButtonText: swalObj.cancelButtonText ?? '아니요'
+  }).then((result) => {
+    if (typeof swalObj.thenFn === 'function') {
+      swalObj.thenFn(result); // 사용자 정의 함수 호출
+    } else {
+      defaultThenFn(); // 기본 함수 호출
+    }
+  });
+};
 app.component('QuillEditor', QuillEditor); // Quill 에디터를 전역 컴포넌트로 등록
 app.use(router);
 app.mount('#app');
--- src/views/board/BoardAddView.vue
+++ src/views/board/BoardAddView.vue
@@ -1,7 +1,52 @@
 <template>
-  <main class="board_add_main"></main>
+  <main class="board_add_main">
+    <section class="board_add_box">
+      <input-text v-model="boardAddObj.title" :ex-text="'제목을 입력하세요'" />
+      <editor @update:value="bindingContent" />
+      <addButton :button-tag="'등록'" @click="boardAdd" />
+    </section>
+  </main>
 </template>
 
-<script setup></script>
+<script setup>
+import editor from '@/components/QuillEditor.vue';
+import addButton from '@/components/ButtonComponent.vue';
+import inputText from '@/components/InputTextComponent.vue';
+import axios from 'axios';
+import { reactive, getCurrentInstance } from 'vue';
+import { useRouter } from 'vue-router';
+
+const router = useRouter();
+const { appContext } = getCurrentInstance();
+const $swalCall = appContext.config.globalProperties.$swalCall;
+const boardAddObj = reactive({
+  title: '',
+  content: ''
+});
+const bindingContent = (value) => {
+  boardAddObj.content = value;
+};
+
+const boardAdd = () => {
+  axios
+    .post('/api/board/add', boardAddObj)
+    .then(({ response }) => {
+      if (response.success) {
+        const swalObj = {
+          title: '성공',
+          text: response.message,
+          icon: 'success',
+          thenFn: () => {
+            router.push('/board/list');
+          }
+        };
+        $swalCall(swalObj);
+      }
+    })
+    .catch((error) => {
+      alert(error.response.data.message);
+    });
+};
+</script>
 
 <style scoped></style>
