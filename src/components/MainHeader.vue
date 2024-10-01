12933f09fd70c33cb20068219f5c8ec6717ea8d1
MungDon <sungminhyeong@naver.com>
2024-10-01 17:42:39 +0900

    헤더 추가

--- src/components/MainHeader.vue
+++ src/components/MainHeader.vue
@@ -1,9 +1,17 @@
 <template>
   <header class="header_con">
-    <section class="header_box"></section>
+    <div class="header_box">
+      <ul class="menu">
+        <li>
+          <RouterLink to="/member/login">로그인</RouterLink>
+        </li>
+      </ul>
+    </div>
   </header>
 </template>
 
-<script setup></script>
+<script setup>
+import { RouterLink } from 'vue-router';
+</script>
 
 <style scoped></style>
